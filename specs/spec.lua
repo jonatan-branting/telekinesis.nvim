51fdc0ffa7b48b3a0d00da99c737b2215cfc9754
Jonatan Branting <jonatan.branting@gmail.com>
2024-06-16 16:23:42 +0200

    Fix busted setup with luarocks

--- specs/spec.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_command [[set rtp+=.]]
-
-vim.opt.swapfile = false
-local cwd = vim.fn.getcwd()
-
-vim.api.nvim_command(string.format([[set rtp+=%s,%s/specs]], cwd, cwd))
-vim.api.nvim_command(string.format([[set packpath=%s/.ci/vendor]], cwd))
-vim.api.nvim_command [[packloadall]]
--- specs/test_spec.lua
+++
@@ -1,7 +0,0 @@
-assert = require "luassert"
-
-describe("example", function()
-  it("can run tests", function()
-    assert.are.same("hello", "hello")
-  end)
-end)
