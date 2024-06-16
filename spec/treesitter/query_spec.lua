da26a614b3abf9ad199201a7a3032f3ea5168a4d
Jonatan Branting <jonatan.branting@gmail.com>
2024-06-16 18:25:33 +0200

    WIP

--- spec/treesitter/query_spec.lua
+++ spec/treesitter/query_spec.lua
@@ -6,16 +6,20 @@ describe("query", function()
   it("returns all nodes matching a given query", function()
     local bufnr = vim.api.nvim_create_buf(false, true)
     local lang = "lua"
-    local query_string = "(_) @node"
+    local query_string = "(parameters) @node"
 
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "def hello", '  puts "Hello, world!"', "end" })
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "local function hello(arg)", '  print("Hello, world!")', "end" })
     vim.api.nvim_buf_call(bufnr, function()
       vim.bo.filetype = lang
     end)
 
     local query = Treesitter.Query:new({ query_string = query_string, bufnr = bufnr })
-    query:map(function(pattern, match, metadata)
-      print(pattern, vim.inspect(match), vim.inspect(metadata))
+
+    local result = query:foreach_node(function(node, content)
+      return content
     end)
+
+    assert.same(1, result:length())
+    assert.same({"(arg)"}, result:first())
   end)
 end)
