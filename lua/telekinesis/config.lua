6c9873f570e5a2016ed05a3d533584ada0d04716
Jonatan Branting <jonatan.branting@gmail.com>
2025-12-16 14:47:02 +0100

    Better default labels (for me)
    Until we make this configurable at least!

--- lua/telekinesis/config.lua
+++ lua/telekinesis/config.lua
@@ -2,7 +2,7 @@ local Config = {}
 
 local default_opts = {
   safe_labels = {"f", "n", "u", "s", "t", "/", "S", "F", "N", "L", "H", "M", "U", "G", "T", "Z", "?"},
-  labels = {"f", "n", "j", "k", "l", "s", "h", "o", "d", "w", "e", "i", "m", "b", "u", "y", "v", "r", "g", "t", "a", "q", "p", "c", "x", "z", "/", "S", "F", "N", "J", "K", "L", "H", "O", "D", "W", "E", "I", "M", "B", "U", "Y", "V", "R", "G", "T", "A", "Q", "P", "C", "X", "Z", "?"}
+  labels = {"f", "n", "j", "k", "d", "h", "e", "i", "m", "b", "u", "y", "v", "r", "g", "t", "o", "l", "s",  "w",  "a", "q", "p", "c", "x", "z", "/", "S", "F", "N", "J", "K", "L", "H", "O", "D", "W", "E", "I", "M", "B", "U", "Y", "V", "R", "G", "T", "A", "Q", "P", "C", "X", "Z", "?"}
 }
 
 Config.default_opts = default_opts
